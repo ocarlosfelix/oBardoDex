223be55b958118aca2d239ca61d5add0fb7d41a5
Carlos Félix <carlos.lima.felix@gmail.com>
2022-03-15 20:04:49 -0300

    Exibição do resultado
    Corrigida a quantidade de casas decimais apresentada no resultado final da operação

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var estruturacheckbox = document.querySelector("input[name=checkestrutura]");
-
 estruturacheckbox.addEventListener("change", function () {
   if (this.checked) {
     estruturachecked.style.display = "inline";
@@ -12,21 +11,21 @@ estruturacheckbox.addEventListener("change", function () {
 });
 
 var transportecheckbox = document.querySelector("input[name=checktransporte]");
-
 transportecheckbox.addEventListener("change", function () {
   if (this.checked) {
     transportechecked.style.display = "inline";
     transporteunchecked.style.display = "none";
+    document.getElementById("custouber").value = "0";
     console.log("Transporte está marcado..");
   } else {
     transportechecked.style.display = "none";
     transporteunchecked.style.display = "inline";
+    document.getElementById("distanciashow").value = "0";
     console.log("Transporte está desmarcado..");
   }
 });
 
 var cachecheckbox = document.querySelector("input[name=checkcache]");
-
 cachecheckbox.addEventListener("change", function () {
   if (this.checked) {
     cachechecked.style.display = "inline";
@@ -39,6 +38,7 @@ cachecheckbox.addEventListener("change", function () {
   } else {
     cachechecked.style.display = "none";
     cacheunchecked.style.display = "inline";
+    document.getElementById("cacheigual").value = "0";
     console.log("Cachê está desmarcado..");
   }
 });
@@ -47,29 +47,31 @@ function valorfinal(event) {
   event.preventDefault();
 
   const custoestrutura = document.getElementById("custoestrutura").value;
+  const custouber = document.getElementById("custouber").value;
   const consumocarro = document.getElementById("consumocarro").value;
   const taxadiasemana = document.getElementById("diasemana").value;
   const tempodeshow = document.getElementById("temposhow").value;
   const price = document.getElementById("precogasolina").value;
   const dist = document.getElementById("distanciashow").value;
   const total = document.getElementById("total");
-
   const cacheartista1 = document.getElementById("cacheartista1").value;
   const cacheartista2 = document.getElementById("cacheartista2").value;
   const cacheartista3 = document.getElementById("cacheartista3").value;
   const cacheartista4 = document.getElementById("cacheartista4").value;
+  const cacheigual = document.getElementById("cacheigual").value;
 
   consumototal =
-    (parseFloat(dist) / parseFloat(consumocarro)) * 2 * parseFloat(price);
+    ((parseFloat(dist) / parseFloat(consumocarro)) * 2 * parseFloat(price)) + parseFloat(custouber);
 
   cachetotal =
     (parseInt(cacheartista1) +
       parseInt(cacheartista2) +
       parseInt(cacheartista3) +
-      parseInt(cacheartista4)) *
+      parseInt(cacheartista4) +
+      parseInt(cacheigual)) *
     parseFloat(tempodeshow);
 
   custototal = parseFloat(consumototal) + parseInt(custoestrutura);
-
-  total.innerHTML = parseFloat(custototal) + parseInt(cachetotal);
+  resultadototal = parseFloat(custototal) + parseInt(cachetotal);
+  total.innerHTML = resultadototal.toFixed([2]);
 }
